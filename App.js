30c809c660f341e92f9a3e69bf0054c08f87ca1c
pallen19 <paulanceallen@live.com>
2022-10-02 19:38:50 -0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import { withAuthenticator, Button, Heading } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+
+/* src/App.js */
+function App({ signOut, user }) {
+  // Todo logic here
+
+  return (
+    <>
+      {/* Add Todo JSX here  */}
+      <Heading level={1}>Hello {user.username}</Heading>
+      <Button onClick={signOut}>Sign out</Button>
+    </>
+  );
+}
+
+export default withAuthenticator(App);
